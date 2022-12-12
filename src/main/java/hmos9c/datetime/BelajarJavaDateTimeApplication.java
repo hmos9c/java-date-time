06c14f1cf3fd5ff3cc8bc8e0dd0f24028c4d10e4
hmos9c <hmos9c@gmail.com>
2022-12-12 18:54:14 +0700

    Membuat Project Java Date Time

---
+++ src/main/java/hmos9c/datetime/BelajarJavaDateTimeApplication.java
@@ -0,0 +1,13 @@
+package hmos9c.datetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaDateTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaDateTimeApplication.class, args);
+	}
+
+}
